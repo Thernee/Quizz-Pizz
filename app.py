d78196dd9f3cab6213d5a20a0a020d79ae14456b
Abubakar Adam Sani <Athernee@gmail.com>
2023-11-19 19:01:23 +0100

    Added initial routes

---
+++ app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask('__name__')
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """Handle hompeage"""
+    return "Welcome to Quizz-pizz. Ready to get intellectually entertained?"
+
+
+@app.route('/start', strict_slashes=False)
+def start_quiz():
+    """Page before quiz starts"""
+    return "You selected __ category and __ difficulty. Start quiz now?"
+
+
+@app.route('/current-quiz', strict_slashes=False)
+def current_quiz():
+    """Active quiz page"""
+    return "Current quiz..."
+
+
+@app.route('/history', strict_slashes=False)
+def past_quizes():
+    """User statistics based on past quizes"""
+    return "Overall score. Average"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
